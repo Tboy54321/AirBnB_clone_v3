71252fbf795b4107c6f4e2f999cb1f84da60d0ab
tboy54321 <akinniranoluwatosin22@gmail.com>
2023-10-29 12:19:29 -0700

    Updated to task-6

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
+"""app_views creation"""
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
@@ -8,6 +8,7 @@ from models import storage
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     return jsonify({"status": "OK"})
+
 
 @app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def get_stats():
