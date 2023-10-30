91585ae3dc7f12a81c61a5d8a11bd40a1fe8d72e
tboy54321 <akinniranoluwatosin22@gmail.com>
2023-10-30 13:09:14 -0700

    Updated amenities.py

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -56,3 +56,15 @@ def update_amenity(amenity_id):
             setattr(amenity, key, value)
     storage.save()
     return jsonify(amenity.to_dict()), 200
+
+
+@app_views.route('/amenities/<amenity_id>',
+                 methods=['DELETE'], strict_slashes=False)
+def delete_amenity(amenity_id):
+    """Deletes a Amenity object"""
+    amenity = storage.get(Amenity, amenity_id)
+    if not amenity:
+        abort(404)
+    storage.delete(amenity)
+    storage.save()
+    return jsonify({}), 200
